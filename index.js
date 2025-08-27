1a1eebb8198871c0bdf8c0de72fca3fc24366745
Mohammad Robiul Hossen <robim6225@gmail.com>
2025-08-28 00:37:59 +0600

    functionality add for call section alert

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// function to get inner text
+//* function to get inner text
 function getInnerText(id) {
   return document.getElementById(id).innerText;
 }
 
-// function for add event listener for increasing heart count
+//* function for add event listener for increasing heart count
 function addEventListenerToIncreaseHeart(id) {
   document.getElementById(id).addEventListener("click", function () {
     let heartCount = Number(getInnerText("heart-count"));
@@ -12,7 +12,8 @@ function addEventListenerToIncreaseHeart(id) {
     console.log("clicked");
   });
 }
-// function calling
+
+//* function calling
 addEventListenerToIncreaseHeart("heart1");
 addEventListenerToIncreaseHeart("heart2");
 addEventListenerToIncreaseHeart("heart3");
@@ -22,3 +23,10 @@ addEventListenerToIncreaseHeart("heart6");
 addEventListenerToIncreaseHeart("heart7");
 addEventListenerToIncreaseHeart("heart8");
 addEventListenerToIncreaseHeart("heart9");
+
+document.getElementById("call1").addEventListener("click", function () {
+  const serviceName = getInnerText("service-name1");
+  const serviceNumber = getInnerText("service-number1");
+  const message = `You are Calling ${serviceName} at ${serviceNumber}`;
+  alert(message);
+});
