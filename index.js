b9c46b99ce70c61430e808cda4a943b054e4ea51
Mohammad Robiul Hossen <robim6225@gmail.com>
2025-08-29 00:08:00 +0600

    add responsive menu with raw javascript

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+//* responsive menu
+document.getElementById("cross-icon").addEventListener("click", function () {
+  document.getElementById("cross-icon").classList.add("hidden");
+  document.getElementById("menu-icon").classList.add("flex");
+  document.getElementById("menu-icon").classList.remove("hidden");
+  const mobileMenu = document.getElementById("mobile-menu");
+  mobileMenu.classList.remove("flex");
+  mobileMenu.classList.add("hidden");
+});
+
+document.getElementById("menu-icon").addEventListener("click", function () {
+  document.getElementById("menu-icon").classList.add("hidden");
+  document.getElementById("cross-icon").classList.add("block");
+  document.getElementById("cross-icon").classList.remove("hidden");
+  const mobileMenu = document.getElementById("mobile-menu");
+  mobileMenu.classList.remove("hidden");
+  mobileMenu.classList.add("flex");
+});
+
 //* function to get inner text
 function getInnerText(id) {
   return document.getElementById(id).innerText;
@@ -41,11 +60,11 @@ function callSectionFunctionality(btnId, serviceId, numberId) {
 
       const cardBodyHtml = ` <div
                 class="flex justify-between items-center gap-2 bg-[#F2F2F2] p-4 rounded-lg mt-4">
-                <div>
+                <div class="">
                   <h2 class="text-[13px] font-semibold mb-1">${serviceName}</h2>
                   <p class="w-full text-[#5C5C5C] text-sm">${serviceNumber}</p>
                 </div>
-                <div class="text-[11px]">${date}</div>
+                <div class="text-[11px] ">${date}</div>
               </div>`;
       document.getElementById("card-body").innerHTML += cardBodyHtml;
 
